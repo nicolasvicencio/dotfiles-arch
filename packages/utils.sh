78ab592d269b2f22b9d7c195875a1fe8490fc63f
Nicolas Vicencio <nicolas.vic.or@protonmail.com>
2022-08-25 20:37:18 -0400

    modifing kitty.conf

---
+++ packages/utils.sh
@@ -0,0 +1,1 @@
+sudo pacman -S onlyoffice-bin yandex-browser discord stremio albert picom-git mongodb-bin mongodb-compass visual-studio-code-bin gvim neovim usb-imager-git kitty redshift android-studio bat lsd cava yay
