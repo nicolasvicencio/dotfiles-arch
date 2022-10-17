d50d8ad664864e2d4a14a92a216787ae4eed4d15
Nicolas Vicencio <nicolas.vic.or@protonmail.com>
2022-10-17 20:28:40 -0300

    adding shutdown script

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+
+if date +"%R" === 20:22  
+then
+ notify-send -u critical "this is a notification test"
+fi
+
+if date +"%R" === 23:50 
+then
+  notify-send -u critical "This pc is gonna shutdown in ten minutes, make sure to save your work done babe"
+
+fireup()
+{  
+  while :; do
+   currenttime=$(date +%H:%M)
+   if [[ "$currenttime" > "00:01" ]] || [[ "$currenttime" < "06:30" ]]; then
+     shutdown now
+   fi
+   test "$?" -gt 128 && break
+  done &
+}
+
+fireup()
+
 shutdown -h 00:00 "The system is shuting down, save your work right now dude, or you just gonna lose everything"
+
+
